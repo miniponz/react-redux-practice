17de2a8d27a742533eb7a485372eea40332acb29
Cara Ponzini <ponzini@gmail.com>
2019-05-23 09:50:10 -0700

    fixed delete coment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import { createStore } from 'redux';
 import reducer from './reducers/index';
+import { addPost } from './actions/blogActions';
+import { addComment, deleteComment } from './actions/commentActions';
 
 const store = createStore(reducer);
 
 console.log(store.getState());
 
 
+store.dispatch(addPost({ title: 'post', body: 'here is a post' }));
+console.log(store.getState());
 
+store.dispatch(addComment('post', 'here is a comment'));
+store.dispatch(addComment('post', 'here is another comment'));
+console.log(store.getState());
+
+
+store.dispatch(deleteComment('post', 0));
+console.log(store.getState());
--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -9,8 +9,9 @@ export default function reducer(state = initialState, action) {
     case CREATE_COMMENT:
       return { ...state, comments: [...state.comments, action.payload] };
     case DELETE_COMMENT:
-      return { ...state, comments: state.comments.filter(comment => comment.postTitle !== action.payload.postTitle && state.comments.indexOf(comment) !== action.payload.commentIndex) };
+      return { ...state, comments: state.comments.filter(comment => comment.postTitle !== action.payload.postTitle || state.comments.indexOf(comment) !== action.payload.commentIndex) };
     default:
       return state;
   }
 }
+
