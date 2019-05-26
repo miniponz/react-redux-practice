0e6924a2b0ceb205525e0e35c5c6461266837987
Cara Ponzini <ponzini@gmail.com>
2019-05-26 13:31:02 -0700

    refactored Create comment still doesn't work.

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -11,12 +11,13 @@ export default function reducer(state = initialState, action) {
     case CREATE_COMMENT:
       // eslint-disable-next-line no-case-declarations
       const id = uuid();
-      // eslint-disable-next-line no-case-declarations
-      const { postId, comment } = action.payload;
-      if(state[postId]) {
-        return { ...state, [postId]: [...state[postId], { id, comment }] };
-      }
-      return { ...state, [postId]: { id, comment } };
+      return {
+        ...state,
+        [action.payload.postId]: [
+          ...(state[action.payload.postId] || []),
+          action.payload.comment, id
+        ]
+      };
     case DELETE_COMMENT:
       return { ...state, comments: state.comments.filter(comment => comment.postTitle !== action.payload.postTitle || state.comments.indexOf(comment) !== action.payload.commentIndex) };
     default:
